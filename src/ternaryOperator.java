d3fdb6a99547f858dcdc9dc77b588688687dd725
Adriz1611 <adrijbhadra@gmail.com>
2025-08-07 10:28:12 +0530

    ternaryoperator and weightconverter

---
+++ src/ternaryOperator.java
@@ -0,0 +1,30 @@
+public class ternaryOperator {
+    public static void main(String[] args) {
+        // ternary operator ? = Return 1 of 2 values if a condition is true
+        // variable = (condition) ? ifTrue : ifFalse;
+
+
+        int score = 50;
+//        if (score >= 60) {
+//            System.out.println("Pass");
+//        }
+//        else{
+//            System.out.println("Fail");
+//        }
+
+        String PasserFail = (score >= 60) ? "Pass" : "Fail";
+        System.out.println(PasserFail);
+
+        int number = 4;
+        String evenOrOdd = ((number % 2) == 0) ? "Even" : "Odd";
+        System.out.println(evenOrOdd);
+
+        int hours = 13;
+        String timeofDay = (hours < 12) ? "A.M" : "P.M";
+        System.out.println(timeofDay);
+
+        int income = 60000;
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+        System.out.println(taxRate);
+    }
+}
---
+++ src/weightConverter.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class weightConverter {
+    public static void main(String[] args) {
+        // WEIGHT CONVERSION PROGRAM
+
+        // Declare variables
+        // welcome message
+        // prompt for user choice
+        // option 1 convert lbs to kgs
+        // option 2 convert kgs to lbs
+        // else print not a valid choice
+
+        Scanner scanner = new Scanner(System.in);
+        double weight;
+        double newWeight;
+        int choice;
+
+        System.out.println("Weight Conversion Program");
+        System.out.println("1:Convert lbs to Kgs");
+        System.out.println("2: Convert kgs to lbs");
+
+        System.out.print("Choose and option : ");
+        choice = scanner.nextInt();
+
+        if (choice == 1) {
+            System.out.println("Enter the weight in lbs : ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 0.453592;
+            System.out.printf("The new weight in kgs is : %.2f", newWeight);
+        } else if (choice == 2) {
+            System.out.println("Enter the weight in kgs : ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 2.20462;
+            System.out.printf("The new weight in lbs is : %.2f", newWeight);
+        } else {
+            System.out.println("Not a valid choice");
+        }
+        scanner.close();
+    }
+}
