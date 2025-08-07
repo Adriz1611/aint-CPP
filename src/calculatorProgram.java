72372049351c1ea15cc01f39ffa8773826c99c6b
Adriz1611 <adrijbhadra@gmail.com>
2025-08-07 11:03:55 +0530

    calciprog and tempconverter

---
+++ src/calculatorProgram.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class calculatorProgram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+
+        System.out.println("Enter the first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Enter an operator ( +, -, *, /, ^ ) : ");
+        operator = scanner.next().charAt(0);
+
+        System.out.println("Enter the second number : ");
+        num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Cannot divide by zero.");
+                    scanner.close();
+                    return;
+                }
+                break;
+
+            case '^':
+                result = Math.pow(num1, num2);
+                break;
+
+            default:
+                System.out.println("Invalid operator.");
+                scanner.close();
+                return;
+        }
+
+        System.out.printf("Result: %.2f\n", result);
+
+        scanner.close();
+    }
+}
---
+++ src/temperatureConverter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class temperatureConverter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.println("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.println("Convert to Celsius or Fahrenheit? (C or F)");
+        unit = scanner.next().toUpperCase();
+
+        newTemp = (unit.equals("C")) ? (temp - 32) * 5 / 9 : (temp * 9 / 5) + 32;
+
+        System.out.println("Converted Temperature: " + newTemp + " degrees " + unit);
+
+        scanner.close();
+    }
+}
